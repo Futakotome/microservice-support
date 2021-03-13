ef0c249696495da941d705a7df846bef0ae4fbf1
futakotome <983528987@qq.com>
2021-03-14 01:02:57 +0800

    package bug

--- starters/distribute-global-id-generator/distribute-global-id-generator-hibernate/src/main/java/io/futakotome/globalId/config/HibernateGlobalIdGeneratorConfiguration.java
+++ starters/distribute-global-id-generator/distribute-global-id-generator-hibernate/src/main/java/io/futakotome/globalId/config/HibernateGlobalIdGeneratorConfiguration.java
@@ -2,9 +2,7 @@ package io.futakotome.globalId.config;
 
 import io.futakotome.globalId.HibernateGlobalIdGenerator;
 import io.futakotome.globalId.IdGenerator;
-import io.futakotome.globalId.ZookeeperGlobalIdGenerator;
 import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
